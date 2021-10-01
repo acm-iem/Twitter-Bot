cd613b78e28d2c2b70f9bfd63f559b800433b8f1
arnab-2001 <arnabbibhuti01@gmail.com>
2021-10-01 12:47:48 +0530

    Updated Readme.md

--- client.js
+++ client.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 
 
 var T = new Twit({
+    // Get the API keys from the developer.twitter.com 
     consumer_key: '505LeLAjIRWU0WkXLr373xPy4',
     consumer_secret: 'DfAcXmT6S7nFk3NpecXJgdW3P07a94nAyQG0eLfXD5TqqLz3Nv',
     access_token: '1432237063743557636-XXvw6HGr2WrVVEDVNlmojdVAzr97kC',
@@ -20,11 +21,16 @@ function responseCallback(err, data, response) {
 
 }
 
+//This stream tracks the specific hashtag or, it can be customised as well
 var stream = T.stream('statuses/filter', { track: '#acmiem' })
 
 stream.on('tweet', function (tweet) {
     console.log(tweet.id)
+
+    //Bot Retweets the Tweet
     T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+
+    //Bot Likes the Tweet
     T.post('favorites/create', { id: tweet.id_str }, responseCallback);
 
 })
