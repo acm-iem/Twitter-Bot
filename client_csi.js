c4c687d1747270e84857ad9272317cb0c0f4bec4
arnab-2001 <arnabbibhuti01@gmail.com>
2021-10-02 13:03:18 +0530

    CSI api keys added

--- client_csi.js
+++ client_csi.js
@@ -8,10 +8,10 @@ var fs = require('fs');
 
 var T = new Twit({
     // Get the API keys from the developer.twitter.com 
-    consumer_key: '505LeLAjIRWU0WkXLr373xPy4',
-    consumer_secret: 'DfAcXmT6S7nFk3NpecXJgdW3P07a94nAyQG0eLfXD5TqqLz3Nv',
-    access_token: '1432237063743557636-XXvw6HGr2WrVVEDVNlmojdVAzr97kC',
-    access_token_secret: 'n5GcLzCOhacYe2xPuIrkp8dda3GDSMaKnf9sdigNzKfYN',
+    consumer_key: '3ByKuUk0qniErJW2XePVmY0cL',
+    consumer_secret: 'cxyVffTFCTFCYZ7JJMmzHwCwIx7DMRtvutLYwlkrMgkgTXlA6u',
+    access_token: '1433328403944333320-39GoYBCNOMd9IsfJr7Ci8ihvS0jEbb',
+    access_token_secret: 'Vl93pLrwxFFIFyCqNj9eN06Vgly9q6PXSaxJ7NCnZbTOp',
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
 });
 
